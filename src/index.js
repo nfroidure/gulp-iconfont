58f0f61c1a829eb316759b5bf26a3423e8795404
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-01-15 12:09:43 +0100

    Updating dependencies, simplifying it all

--- src/index.js
+++ src/index.js
@@ -7,106 +7,17 @@ var gutil = require('gulp-util')
   , ttf2woff = require('gulp-ttf2woff')
 ;
 
-function group() {
-  var pass = new Stream.PassThrough({objectMode: true});
-
-  // Mapping given streams
-  var streams = [].slice.call(arguments,0).map(function(stream) {
-    // Listening to streams datas
-    stream.on('data', function(file) {
-      pass.write(file);
-    });
-    // Listening to streams end
-    stream.once('end', function() {
-      streams.splice(streams.indexOf(stream), 1);
-      if(!streams.length) {
-        pass.end();
-      }
-    });
-    return stream;
-  });
-
-  return pass;
-  
-}
-
-function throughs() {
-
-  var trans = new Stream.Transform({objectMode: true});
-
-  // Repeat content to each stream
-  trans._transform = function (file, unused, cb) {
-    streams.forEach(function(stream) {
-      stream.write(file.clone());
-    });
-    trans.push(file);
-    cb();
-  };
-
-  // Ending all through streams when flushing
-  trans._flush = function (cb) {
-    streams.forEach(function(stream) {
-      stream.end();
-    });
-    cb();
-  };
-
-  // Mapping given streams
-  var streams = [].slice.call(arguments,0).map(function(stream) {
-    // Listening to streams datas
-    stream.on('data', function(file) {
-      trans.push(file);
-    });
-    return stream;
-  });
-
-  return trans;
-
-}
-
-function explode() {
-
-  var trans = new Stream.Transform({objectMode: true});
-
-  // Repeat content to each stream
-  trans._transform = function (file, unused, cb) {
-    streams.forEach(function(stream) {
-      stream.write(file.clone());
-    });
-    trans.push(file);
-    cb();
-  };
-
-  // Ending all through streams when flushing
-  trans._flush = function (cb) {
-    streams.forEach(function(stream) {
-      stream.end();
-    });
-    cb();
-  };
-
-  // Mapping given streams
-  var streams = [].slice.call(arguments,0).map(function(stream) {
-    return stream;
-  });
-
-  return trans;
-
-}
-
 function gulpFontIcon(options) {
-  var svgOut = new Stream.PassThrough({objectMode: true});
-  var consumer = new Stream.PassThrough({objectMode: true});
-
   // Generating SVG font and saving her
-  var svg = svgicons2svgfont(options);
-  var main = svg.pipe(explode(svgOut))
+  var inStream = svgicons2svgfont(options);
   // Generating TTF font and saving her
-    .pipe(svg2ttf())
-  // Generation EOT and WOFF fonts
-    .pipe(throughs(ttf2eot(), ttf2woff()));
+  var outStream = inStream.pipe(svg2ttf({clone: true}))
+  // Generating EOT font
+    .pipe(ttf2eot({clone: true}))
+  // Generating WOFF font
+    .pipe(ttf2woff({clone: true}));
 
-  return duplexer(svg, group(svgOut, main));
+  return duplexer(inStream, outStream);
 
 }
 
