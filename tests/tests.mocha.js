3d7a6773db17d19f93ca62de045855cf5b16a16c
Nicolas Froidure <nicolas.froidure@insertafter.com>
2015-06-07 12:44:03 +0200

    Remove extra gulp fixed bugs code

--- tests/tests.mocha.js
+++ tests/tests.mocha.js
@@ -37,32 +37,27 @@ describe('gulp-iconfont', function() {
         }))
         .pipe(gulp.dest(__dirname+'/results/'))
         .pipe(es.wait(function() {
-          // Trick to wait for datas beeing written to disk...
-          // https://github.com/wearefractal/vinyl-fs/issues/7
-          setTimeout(function() {
-            assert.equal(
-              fs.readFileSync(__dirname+'/results/iconsfont.svg', 'utf8'),
-              fs.readFileSync(__dirname+'/expected/iconsfont.svg', 'utf8')
-            );
-            assert.equal(
-              fs.readFileSync(__dirname+'/results/iconsfont.ttf', 'utf8'),
-              fs.readFileSync(__dirname+'/expected/iconsfont.ttf', 'utf8')
-            );
-            assert.equal(
-              fs.readFileSync(__dirname+'/results/iconsfont.eot', 'utf8'),
-              fs.readFileSync(__dirname+'/expected/iconsfont.eot', 'utf8')
-            );
-            assert.equal(
-              fs.readFileSync(__dirname+'/results/iconsfont.woff', 'utf8'),
-              fs.readFileSync(__dirname+'/expected/iconsfont.woff', 'utf8')
-            );
-            done();
-          }, 3000);
+          assert.equal(
+            fs.readFileSync(__dirname+'/results/iconsfont.svg', 'utf8'),
+            fs.readFileSync(__dirname+'/expected/iconsfont.svg', 'utf8')
+          );
+          assert.equal(
+            fs.readFileSync(__dirname+'/results/iconsfont.ttf', 'utf8'),
+            fs.readFileSync(__dirname+'/expected/iconsfont.ttf', 'utf8')
+          );
+          assert.equal(
+            fs.readFileSync(__dirname+'/results/iconsfont.eot', 'utf8'),
+            fs.readFileSync(__dirname+'/expected/iconsfont.eot', 'utf8')
+          );
+          assert.equal(
+            fs.readFileSync(__dirname+'/results/iconsfont.woff', 'utf8'),
+            fs.readFileSync(__dirname+'/expected/iconsfont.woff', 'utf8')
+          );
+          done();
         }));
     });
 
     it('should emit an event with the codepoint mapping', function(done) {
-      this.timeout(5000);
       var codepoints;
       gulp.src(__dirname+'/fixtures/iconsfont/*.svg', {buffer: false})
         .pipe(iconfont({
@@ -72,15 +67,11 @@ describe('gulp-iconfont', function() {
         })
         .pipe(gulp.dest(__dirname + '/results/'))
         .pipe(es.wait(function() {
-          // Trick to wait for datas beeing written to disk...
-          // https://github.com/wearefractal/vinyl-fs/issues/7
-          setTimeout(function() {
-            neatequal(codepoints,
-              JSON.parse(fs.readFileSync(
-                __dirname + '/expected/codepoints.json', 'utf8'))
-            );
-            done();
-          }, 3000);
+          neatequal(codepoints,
+            JSON.parse(fs.readFileSync(
+              __dirname + '/expected/codepoints.json', 'utf8'))
+          );
+          done();
         }));
     });
 
