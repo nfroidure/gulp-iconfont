995ff72d3e90653adcd6325926901897e05c7235
Raphael Schweikert <Raphael.Schweikert1@swisscom.com>
2015-06-30 10:47:59 +0200

    test case for https://github.com/nfroidure/gulp-iconfont/issues/64

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var fs = require('fs');
+
+var gulp = require('gulp');
+var iconfont = require('gulp-iconfont');
+
+var fonts = fs.readdirSync(__dirname + '/fonts');
+
+
+fonts.forEach(function(font) {
+	var info = require('./fonts/' + font + '/info');
+	var defaultMetadataProvider = require('svgicons2svgfont/src/metadata')();
+	function metadataProvider(path, callback) {
+		defaultMetadataProvider(path, function(error, metadata) {
+			if('ligatures' in info && metadata.name in info.ligatures) {
+				info.ligatures[metadata.name].forEach(function(ligature) {
+					if(metadata.unicode.indexOf(ligature) === -1) {
+						metadata.unicode.push(ligature);
+					}
+				});
+			}
+			callback(error, metadata);
+		});
+	}
+	gulp.task(font, function() {
+		gulp.src(['fonts/' + font + '/*.svg'])
+		.pipe(iconfont({
+			fontName: info.name, // required
+			appendCodepoints: true, // recommended option
+			fontHeight: info.fontHeight,
+			normalize: !!info.normalize,
+			svg: true,
+			round: 100,
+			metadataProvider: metadataProvider
+		}))
+		.pipe(gulp.dest(info.destination));
+	});
+});
+
+gulp.task('default', fonts);
