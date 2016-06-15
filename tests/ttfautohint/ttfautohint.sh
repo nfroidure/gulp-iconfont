8eaf91a1a21fd4b9967ebe1347206f732e06b25c
Raphael Schweikert <Raphael.Schweikert1@swisscom.com>
2016-06-15 20:40:00 +0200

    Try rootless travis w/o docker

--- tests/ttfautohint/ttfautohint.sh
+++ tests/ttfautohint/ttfautohint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-exec docker run --rm -a STDIN -a STDOUT -a STDERR -i ttfautohint:1.5 "$@"
+if command -v docker >/dev/null 2>&1; then
+	exec docker run --rm -a STDIN -a STDOUT -a STDERR -i ttfautohint:1.5 "$@"
+else
+	exec ttfautohint "$@"
+fi
