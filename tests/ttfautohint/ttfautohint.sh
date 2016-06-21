fc85c59dae740c22e77537de09c2780ab136cd3a
Raphael Schweikert <any@sabberworm.com>
2016-06-21 11:32:54 +0200

    Also check if docker image for ttfautohint exists
    So that the tests won’t fail when the `docker` command becomes available on non-root machines.

--- tests/ttfautohint/ttfautohint.sh
+++ tests/ttfautohint/ttfautohint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-if command -v docker >/dev/null 2>&1; then
-	exec docker run --rm -a STDIN -a STDOUT -a STDERR -i ttfautohint:1.5 "$@"
+DOCKER_IMAGE="ttfautohint:1.5"
+if command -v docker >/dev/null 2>&1 && test ! -z "$(docker images -q "$DOCKER_IMAGE" 2> /dev/null)"; then
+	exec docker run --rm -a STDIN -a STDOUT -a STDERR -i "$DOCKER_IMAGE" "$@"
 else
 	exec ttfautohint "$@"
 fi
